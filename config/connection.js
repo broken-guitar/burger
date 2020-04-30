76d56ded84df9d443a1d4b79d18db973fc8dea98
David Flores <drfloresj@gmail.com>
2020-04-29 23:13:09 -0700

    added heroku deployment configuration

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
 // load mysql library
-
 var mysql = require("mysql");
+var connection;
 
 // set up connection to database
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+};
 
 // open connection
 connection.connect(function(err) {
--- db/seeds.sql
+++ db/seeds.sql
@@ -2,4 +2,4 @@ USE burgers_db;
 -- 'devoured' defaults to 'false'
 INSERT INTO burgers (burger_name) VALUES ('Californian');
 INSERT INTO burgers (burger_name) VALUES ('Inferno');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Based Vegan', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Based Vegan Goddess', false);
